135a451557d4a229311798139d57117228eb13bc
Wendy <tianyuan.han@hatmail.com>
2025-05-31 14:48:57 -0700

    basic javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "Wendy";
+let age = 14;
+let likesSushi = true;
+
+function greeting(name) {
+    console.log("Hello", name);
+}
+
+greeting("Wendy");
+
+let newName = document.getElementById("greeter").value;
